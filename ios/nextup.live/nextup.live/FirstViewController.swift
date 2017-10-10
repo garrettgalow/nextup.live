894d7b4a9fda8efc78fada2efb84e11509591f6e
Garrett Galow <garrett.galow@gmail.com>
2017-10-09 21:55:34 -0700

    ios project files

---
+++ ios/nextup.live/nextup.live/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  nextup.live
+//
+//  Created by Garrett Galow on 10/9/17.
+//  Copyright © 2017 Garrett Galow. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
